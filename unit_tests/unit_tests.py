7c889c85db6be3427ed3ca8b549c1aa2eb0b048e
Agniv Adhikari <agniv.others@gmail.com>
2019-10-02 12:24:04 +0530

    Changed error messages.

--- unit_tests/unit_tests.py
+++ unit_tests/unit_tests.py
@@ -18,11 +18,11 @@ class TelegraphHomePageTest(unittest.TestCase):
     def test_access_a_particular_date(self):
         browser = self.ts.get_browser()
         browser = self.ts.access_archive_of_date(browser,"2019","10","01")
-        self.assertEqual('Telegraph india epaper Calcutta 01 Oct 2019 | Page 1',browser.title,msg="The Title did not match")
+        self.assertEqual('Telegraph india epaper Calcutta 01 Oct 2019 | Page 1',browser.title,msg="The Title for a particular date did not match")
     def test_access_a_particular_date_and_page_no(self):
         browser = self.ts.get_browser()
         browser = self.ts.access_archive_of_date_and_page_no(browser,"2019","10","01",10)
-        self.assertEqual('Telegraph india epaper Calcutta 01 Oct 2019 | Page 10',browser.title,msg="The Title did not match")
+        self.assertEqual('Telegraph india epaper Calcutta 01 Oct 2019 | Page 10',browser.title,msg="The Title or page number for a particular date and page number did not match")
 
 if __name__ == '__main__':
     unittest.main() 
